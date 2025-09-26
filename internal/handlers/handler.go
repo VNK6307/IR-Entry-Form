c0aa1e936fa1507391619322141de24ddf86df0a
VNK6307 <vnkushchenko@gmail.com>
2025-09-26 19:45:07 +0300

    Ready to handling updates

--- internal/handlers/handler.go
+++ internal/handlers/handler.go
@@ -25,10 +25,19 @@ func (handler *Handler) HandleUpdate(upd models.Update) {
 func (handler *Handler) handleMessage(message *models.Message) {
 	//ToDo Realize me!
 	chatID, text, msgID := message.Chat.ID, message.Text, message.MessageID
-	// ToDo Change to switch
-	if text == "/personal" {
-		println("Здесь надо начинать обработку сообщений бота")
-		fmt.Printf("ChatID: %d \n MessageId: %d \n", chatID, msgID)
+
+	fmt.Println("ChatID:", chatID, "Text:", text, "MessageID:", msgID)
+
+	switch text {
+	case "/personal":
+		fmt.Println("Здесь будет заполнение личной заявки.") //ToDo Realize me!
+	case "/team":
+		fmt.Println("Здесь будет заполнение командной заявки.") //ToDo Realize me!
+	case "/list":
+		fmt.Println("Здесь будет реализован вывод списка заявленных пилотов.") //ToDo Realize me!
+	default:
+		fmt.Println("Здесь будет реализован ответ в случае получения неизвестной команды.") //ToDo Realize me!
+
 	}
 
 }
